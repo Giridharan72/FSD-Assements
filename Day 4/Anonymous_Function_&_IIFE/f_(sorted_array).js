b7a412e2ecc7f23a55820d06989b3ebf7406fc21
Giridharan72 <113990195+Giridharan72@users.noreply.github.com>
2023-11-29 00:09:10 +0530

    f_(sorted_array).js

--- Day 4/Anonymous_Function_&_IIFE/f_(sorted_array).js
+++ Day 4/Anonymous_Function_&_IIFE/f_(sorted_array).js
@@ -1,17 +1,49 @@
 (() => {
-    let numbers = [11, 24, 13, 44, 5, 61, 7, 18, 19, 10];
-    function swap(numbers,index1,index2){
-        let temp=numbers[index1];
-        numbers[index1]=numbers[index2];
-        numbers[index2]=temp;
+    function mergeArrays(arr1, arr2) {
+        let mergedArray = [];
+        let i = 0, j = 0;
 
-        }
-        for(let i=0;i<numbers.length-1;i++){
-            for(let j=0;j<numbers.length-i-1;j++){
-                if(numbers[j]>numbers[j+1]){
-                    swap(numbers,j,j+1);
-                }
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] <= arr2[j]) {
+                mergedArray.push(arr1[i]);
+                i++;
+            } else {
+                mergedArray.push(arr2[j]);
+                j++;
             }
         }
-        console.log(numbers);
+
+        // Add remaining elements from arr1
+        while (i < arr1.length) {
+            mergedArray.push(arr1[i]);
+            i++;
+        }
+
+        // Add remaining elements from arr2
+        while (j < arr2.length) {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+
+        return mergedArray;
+    }
+
+    let numbers1 = [11, 24, 44, 61, 65];
+    let numbers2 = [5, 7, 18, 19, 27, 30];
+
+    // Merge the arrays
+    let mergedArray = mergeArrays(numbers1, numbers2);
+
+    // Calculate the median
+    let n = mergedArray.length;
+    let median;
+
+    if (n % 2 === 0) {
+        median = (mergedArray[n / 2 - 1] + mergedArray[n / 2]) / 2;
+    } else {
+        median = mergedArray[Math.floor(n / 2)];
+    }
+
+    console.log("Merged Array:", mergedArray);
+    console.log("Median:", median);
 })();
